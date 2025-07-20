e1e3b4f664d56a4cde14ccce416047f1cc4d60de
Sameer Rolly <149786455+sameerrolly@users.noreply.github.com>
2025-07-20 20:05:30 +0530

    Update settings.py

--- Eco_nature/settings.py
+++ Eco_nature/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'drugabuse22@gmail.com'
-EMAIL_HOST_PASSWORD = 'drug_abuse'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
 
